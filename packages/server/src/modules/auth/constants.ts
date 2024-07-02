4ef17a441bab7b76079917ae8f8c145981d6f41c
tech-zjf <907572438@qq.com>
2024-07-02 17:17:06 +0800

    feat: 添加白名单

--- packages/server/src/modules/auth/constants.ts
+++ packages/server/src/modules/auth/constants.ts
@@ -11,6 +11,24 @@ export const WHITE_LIST = new Map([
             wechatAvatarUrl: 'https://p6-passport.byteacctimg.com/img/user-avatar/8677e25c5256376141eb87cf855cd9fa~80x80.awebp',
         },
     ],
+    [
+        '13636076119',
+        {
+            name: '海金哥',
+            password: '660803',
+            description: '海金',
+            wechatAvatarUrl: 'https://p6-passport.byteacctimg.com/img/user-avatar/8677e25c5256376141eb87cf855cd9fa~80x80.awebp',
+        },
+    ],
+    [
+        '15971241618',
+        {
+            name: '人王',
+            password: '000823',
+            description: '艳',
+            wechatAvatarUrl: 'https://p6-passport.byteacctimg.com/img/user-avatar/8677e25c5256376141eb87cf855cd9fa~80x80.awebp',
+        },
+    ],
 ]);
 
 export const JWT_CONFIG = {
