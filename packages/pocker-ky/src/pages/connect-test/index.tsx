6c095eb278a698b27b611bc19e8e7abd67ea0b68
tech-zjf <907572438@qq.com>
2024-07-11 14:11:58 +0800

    fix: but

--- packages/pocker-ky/src/pages/connect-test/index.tsx
+++ packages/pocker-ky/src/pages/connect-test/index.tsx
@@ -1,47 +1,10 @@
-import useSocket from '@/libs/hooks/use-socket';
 import { Button } from 'antd';
-import { useEffect, useRef, useState } from 'react';
-
-const useTest = (s: number) => {
-    const [currentS, setCurrentS] = useState(s);
-    const [isStart, setIsStart] = useState(false);
-    useEffect(() => {
-        let timer: NodeJS.Timeout | null = null;
-        if (!isStart) {
-            if (timer) {
-                clearInterval(timer);
-            }
-            return;
-        }
-        timer = setInterval(() => {
-            if (currentS === 0) {
-                clearInterval(timer as NodeJS.Timeout);
-                return;
-            }
-            setCurrentS(currentS - 1);
-        }, 1000);
-        return () => {
-            clearInterval(timer);
-        };
-    }, [currentS, isStart]);
-    return {
-        currentS,
-        stop: () => {
-            setIsStart(false);
-        },
-        start: () => {
-            setIsStart(true);
-        }
-    };
-};
+import { useState } from 'react';
 
 const ConnectTest: React.FC = () => {
-    const { socket } = useSocket();
     const [count, setCount] = useState(1);
-    const { currentS, start, stop } = useTest(10);
     return (
         <>
-            <p>{currentS}</p>
             <Button
                 type="primary"
                 key={1}
@@ -50,12 +13,6 @@ const ConnectTest: React.FC = () => {
                 }}
             >
                 点击
-            </Button>
-            <Button type="primary" key={2} onClick={start}>
-                开始
-            </Button>
-            <Button type="primary" key={3} onClick={stop}>
-                暂停
             </Button>
         </>
     );
---
+++ tree.md
@@ -0,0 +1,53 @@
+packages
+├── pocker-ky
+│   ├── .eslintrc.cjs
+│   ├── .gitignore
+│   ├── .prettierrc
+│   ├── env
+│   │   ├── .env.beta
+│   │   └── .env.prod
+│   ├── index.html
+│   ├── package-lock.json
+│   ├── package.json
+│   ├── postcss.config.cjs
+│   ├── public
+│   │   └── image.png
+│   ├── src
+│   │   ├── .DS_Store
+│   │   ├── App.tsx
+│   │   ├── api
+│   │   │   ├── .DS_Store
+│   │   │   ├── constant.ts
+│   │   │   ├── http.ts
+│   │   │   ├── index.ts
+│   │   │   ├── interface.ts
+│   │   │   └── modules
+│   │   ├── assets
+│   │   │   ├── css
+│   │   │   └── images
+│   │   ├── components
+│   │   │   ├── custom-icon
+│   │   │   ├── interface.ts
+│   │   │   ├── login-card
+│   │   │   └── ui
+│   │   ├── constants
+│   │   │   └── dayjs.ts
+│   │   ├── index.less
+│   │   ├── libs
+│   │   │   ├── .DS_Store
+│   │   │   ├── hooks
+│   │   │   ├── storage
+│   │   │   └── tools
+│   │   ├── main.tsx
+│   │   ├── pages
+│   │   │   ├── connect-test
+│   │   │   ├── home
+│   │   │   ├── login
+│   │   │   └── room
+│   │   ├── router
+│   │   │   └── index.tsx
+│   │   └── vite-env.d.ts
+│   ├── tailwind.config.js
+│   ├── tsconfig.json
+│   ├── tsconfig.node.json
+│   └── vite.config.ts
