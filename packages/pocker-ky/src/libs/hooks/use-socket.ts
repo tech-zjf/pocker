610df36756189f38b49e701fd6267f40b567144f
tech-zjf <907572438@qq.com>
2024-07-25 17:16:09 +0800

    fix: bug

--- packages/pocker-ky/src/libs/hooks/use-socket.ts
+++ packages/pocker-ky/src/libs/hooks/use-socket.ts
@@ -32,22 +32,26 @@ export enum EventListenerEnum {
     PUSH_ROOM_INFO = 'refresh-room-info'
 }
 
-const useSocket = () => {
-    const socket = io('http://110.40.198.126:8888/game', {
-        extraHeaders: {
-            Authorization: getToken()
-        },
-        autoConnect: false
-    });
-    socket.connect();
+let socketInstance: Socket | null = null;
 
-    socket.on('connect_error', (err) => {
-        setToken();
-        setUserInfo();
-        redirect('/login');
-    });
+const useSocket = () => {
+    if (!socketInstance) {
+        socketInstance = io('http://110.40.198.126:8888/game', {
+            extraHeaders: {
+                Authorization: getToken()
+            },
+            autoConnect: false
+        });
+        socketInstance.connect();
+
+        socketInstance.on('connect_error', (err) => {
+            setToken();
+            setUserInfo();
+            redirect('/login');
+        });
+    }
     return {
-        socket
+        socket: socketInstance
     };
 };
 export default useSocket;
