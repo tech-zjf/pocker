33c1b1a85e507735accf41638e3fa55b32bcbaef
tech-zjf <907572438@qq.com>
2024-08-13 14:21:43 +0800

    chore: delete

--- packages/pocker-ky/src/router/index.tsx
+++ packages/pocker-ky/src/router/index.tsx
@@ -5,7 +5,6 @@ const Home = lazy(() => import('@/pages/home'));
 const Room = lazy(() => import('@/pages/room'));
 const App = lazy(() => import('@/App'));
 const Login = lazy(() => import('@/pages/login'));
-const ConnectTest = lazy(() => import('@/pages/connect-test'));
 const router = createBrowserRouter([
     {
         path: '/',
@@ -14,10 +13,6 @@ const router = createBrowserRouter([
             {
                 index: true,
                 element: <Home />
-            },
-            {
-                path: '/connect-test',
-                element: <ConnectTest />
             }
         ]
     },
